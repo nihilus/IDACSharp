db5d0754ec9de83e836d9d4356b8f1ae4988ab7f
Fabian <fabian@arctium.org>
2015-06-01 18:31:22 +0200

    Added source for the sample plugin.

---
+++ samples/IDATest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using IDACSharp;
+
+namespace IDATest
+{
+    public class CPlugin : IPlugin
+    {
+        public static int StartPlugin()
+        {
+            //Function func=new Function(
+            try
+            {
+                FrmMain f = new FrmMain();
+                //f.ShowDialog(new IDAHWND(hwnd));
+                //f.ShowDialog();
+                f.Show();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                //XTrace.WriteLine(ex.ToString());
+                MessageBox.Show(ex.Message);
+                return -1;
+            }
+        }
+
+
+
+        public bool Init()
+        {
+            return true;
+        }
+
+        public string Name
+        {
+            get { return "IDA Test"; }
+            set { }
+        }
+
+        //FrmMain frm;
+
+        public void Start()
+        {
+            FrmMain frm = null;
+
+            if (frm == null)
+            {
+                frm = new FrmMain();
+                frm.Show();
+            }
+            else
+                frm.BringToFront();
+        }
+
+        public void Term()
+        {
+        }
+
+    }
+}
